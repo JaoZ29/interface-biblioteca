b5f93e94004cb1197abf3f017c1c964bfcd1d18c
Joao Gabriel Sabino De Araujo <joaogabrielsraujo@gmail.com>
2025-03-19 16:47:03 -0300

    Criando o login

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Cabecalho from './components/Cabecalho/Cabecalho';
+import Rodape from './components/Cabecalho/Rodape/Rodape';
+import Welcome from './components/Cabecalho/Welcome/Welcome';
+function App() {
+
+  return (
+    <>
+      <Cabecalho />
+      <Welcome />
+      <Rodape />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cabecalho/Rodape/Rodape.tsx
@@ -0,0 +1,12 @@
+import estilo from './Rodape.module.css';
+
+function Rodape() {
+    return (
+        <footer className={estilo.rodape}>
+            <p>Desenvolvido por:Jao Gabriel</p>
+            <p>Copyright</p>
+        </footer>
+    );
+}
+
+export default Rodape;
